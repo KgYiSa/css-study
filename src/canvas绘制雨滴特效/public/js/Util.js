6b979705e44b036f618e4eadca10682cb9cf82a6
KgYiSa <kuangguanlong@163.com>
2017-04-27 16:16:46 +0800

    canvas绘制雨滴特效

---
+++ src/canvas绘制雨滴特效/public/js/Util.js
@@ -0,0 +1,16 @@
+// 获取页面的宽度 window.innerWidth
+function getPageWidth() {
+    return window.innerWidth;
+}
+// 获取页面的高度 window.innerHeight
+function getPageHeight() {
+    return window.innerHeight;
+}
+// clientWidth
+function getViewportWidth() {
+    return document.body.clientWidth;
+}
+// clientWidth
+function getViewportHeight() {
+    return document.body.clientHeight;
+}
